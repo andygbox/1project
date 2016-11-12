5f7d96a07324585a9892fc323054f2d91ee3d2f2
andy.g <andygbox112@gmail.com>
2016-11-12 16:02:01 +0500

    add task2b to index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
-  res.json({
+  	res.json({
     hello: 'Skill-Branch',
   });
 });
@@ -14,6 +14,28 @@ app.get('/task2a', (req, res) => {
 	res.send(sum.toString());
 });
 
+/**
+	Try task 2B
+*/
+app.get('/task2b', (req, res) => {
+	let lname, shortname;
+	let fullname = req.query.fullname.toUpperCase() || '';
+  	let init = fullname ? fullname.split(" ").filter( el => el ) : 0;
+
+  	if (init && init.length <= 3
+    	&& init.every( el => !/[\d_\/]+/i.test(el))
+    	) {
+    	lname = init.pop();
+    	lname = lname[0] + lname.slice(1).toLowerCase();
+    	init = init.map( el => `${el[0]}.` );
+    	shortname = [lname, ...init].join(" ");
+  	} else {
+    	shortname = "Invalid fullname";
+  	}
+
+ 	res.send(shortname);
+});
+
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+  	console.log('Your app listening on port 3000!');
 });
